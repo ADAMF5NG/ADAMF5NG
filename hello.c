69ee6217c9f44be77385f53541c2c008cf64fc33
ADAMF5NG <143734963+ADAMF5NG@users.noreply.github.com>
2023-09-01 02:32:06 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+/*-------------------------
+Your name   = Adam Feng
+Your SBU ID = 115202946
+Your NetID  = atfeng 
+-----------------------------*/
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!\n");
+   return 0;
+}
