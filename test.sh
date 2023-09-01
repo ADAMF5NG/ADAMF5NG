13a987584856b01b4959d012909b37d7a98e0294
ADAMF5NG <143734963+ADAMF5NG@users.noreply.github.com>
2023-09-01 18:52:15 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ echo
 output=$(./a.out)
 expected_output="Hello, World!"
 
-if [ $? -eq 0 ] ; then
+if [ $? -eq 0 ]; then
   echo "Pass: Program exited zero"
 else
   echo "Fail: Program did not exit zero"
   exit 1
 fi
 
-if [ "$output" == "$expected_output" ] ; then
+if [ "$output" == "$expected_output" ]; then
   echo "Pass: Output is correct"
 else
   echo "Expected '$expected_output' but got: $output"
